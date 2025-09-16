a50c4908fe9711654806da116b99f519ada665ad
iqraafatima <syedaaiqraa1308@gmail.com>
2025-09-16 22:59:09 +0530

    second

---
+++ reference variables.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=45;
+    cout<<x<<endl;
+}
---
+++ reference_variables.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=45;
+    int &y=x;
+    cout<<x<<endl<<y;
+}
